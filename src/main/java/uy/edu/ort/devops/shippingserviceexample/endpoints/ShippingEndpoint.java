edc8a4dbb95d63f322ccab1812027f86f2c2a253
HRCFranciscoFontoura <ffontoura@hoodriverconsulting.com>
2024-12-06 22:04:47 -0300

    Adding Healthcheck endpoints

--- src/main/java/uy/edu/ort/devops/shippingserviceexample/endpoints/ShippingEndpoint.java
+++ src/main/java/uy/edu/ort/devops/shippingserviceexample/endpoints/ShippingEndpoint.java
@@ -7,19 +7,19 @@ import uy.edu.ort.devops.shippingserviceexample.domain.Shipping;
 import uy.edu.ort.devops.shippingserviceexample.logic.ShippingLogic;
 
 @RestController
-@RequestMapping("/shipping")
+@RequestMapping
 public class ShippingEndpoint {
 
     @Autowired
     private ShippingLogic logic;
 
-    @PostMapping(path = "/{id}")
+    @PostMapping(path = "/shipping/{id}")
     public ResponseEntity create(@PathVariable("id") String id) {
         logic.addShipping(id);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/shipping/{id}")
     public ResponseEntity getProduct(@PathVariable("id") String id) {
         if (logic.hasShipping(id)) {
             Shipping shipping = logic.getShipping(id);
@@ -27,5 +27,10 @@ public class ShippingEndpoint {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/healthcheck")
+    public String healthCheck() {
+        return "Health Passed";
     }
 }
